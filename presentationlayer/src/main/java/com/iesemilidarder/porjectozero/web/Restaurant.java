d00bd57ffbfa3573bfdbf92b1db8afb675073a8d
monterogarcia1000 <monterogarcia1000@gmail.com>
2018-01-16 17:39:33 +0100

    Algo

---
+++ presentationlayer/src/main/java/com/iesemilidarder/porjectozero/web/Restaurant.java
@@ -0,0 +1,66 @@
+package com.iesemilidarder.porjectozero.web;
+// Objeto para guardar los restaurantes
+
+public class Restaurant {
+
+    private String nombre;
+
+    private  String direccion;
+
+    private String web;
+
+    private  String telefono;
+
+    private String tipo;
+
+    public String getUrl_imagen() {
+        return url_imagen;
+    }
+
+    public void setUrl_imagen(String url_imagen) {
+        this.url_imagen = url_imagen;
+    }
+
+    private String url_imagen;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
+
